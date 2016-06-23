c3158ac69718462014a7bf7e0982b2eebe093591
Ryan Zhou <ryanzhou7@gmail.com>
2016-06-23 13:37:23 -0700

    add spinner array & clean for sub

--- app/src/main/java/com/ryanzhou/company/miwok/MainActivity.java
+++ app/src/main/java/com/ryanzhou/company/miwok/MainActivity.java
@@ -5,14 +5,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    public final String LOG_TAG = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.buttonExplore)
+    public void clickExplore(View view) {
+        startActivity(new Intent(getApplicationContext(), ExploreWordActivity.class));
     }
 
     @OnClick(R.id.buttonQuickLookup)
@@ -20,11 +28,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(), LookupWordActivity.class));
     }
 
-    @OnClick(R.id.buttonExplore)
-    public void clickExplore(View view) {
-        startActivity(new Intent(getApplicationContext(), ExploreWordActivity.class));
-
-    }
 
     @OnClick(R.id.buttonSubmit)
     public void clickSubmit(View view) {
--- app/src/main/java/com/ryanzhou/company/miwok/SubmitWordActivity.java
+++ app/src/main/java/com/ryanzhou/company/miwok/SubmitWordActivity.java
@@ -1,16 +1,33 @@
 package com.ryanzhou.company.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SubmitWordActivity extends AppCompatActivity {
 
     /* Would look to see if the word is already in db. If not, then user would choose category
     * for word to be submitted. Then user would record & play using android android.media. Finally,
-    * submission would be made to the cloud*/
+    * submission would be made to the cloud.*/
+    @BindView(R.id.spinner)
+    Spinner mCategorySpinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
+        ButterKnife.bind(this);
+        setAdapterforCategorySpinner();
+    }
+
+    private void setAdapterforCategorySpinner() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.word_categories_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mCategorySpinner.setAdapter(adapter);
     }
 }
